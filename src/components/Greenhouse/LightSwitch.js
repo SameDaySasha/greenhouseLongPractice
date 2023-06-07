bc13583411900021491f0ad3c46f842b664d8ecc
ansorokey <ansorokey@gmail.com>
2023-06-07 18:45:05 -0400

    button changes day/night

--- src/components/Greenhouse/LightSwitch.js
+++ src/components/Greenhouse/LightSwitch.js
@@ -1,10 +1,16 @@
 import './LightSwitch.css';
+import { useTheme } from '../../context/ThemeContext';
 
 function LightSwitch() {
+  const { themeName, setThemeName } = useTheme();
+  const changeTheme = (e) => {
+    setThemeName(e.target.innerText.toLowerCase());
+    console.log(e.target.innerText.toLowerCase());
+  }
   return (
-    <div className="light-switch day">
-      <div className="on">DAY</div>
-      <div className="off">NIGHT</div>
+    <div className={`light-switch ${themeName}`}>
+      <div onClick={changeTheme} className="on">DAY</div>
+      <div onClick={changeTheme} className="off">NIGHT</div>
     </div>
   );
 }
