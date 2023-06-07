a94675e69488375a6d34ef7edd48a8c18c4f9a93
ansorokey <ansorokey@gmail.com>
2023-06-07 18:51:52 -0400

    created climateCOntext

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -1,2 +1,17 @@
+import { createContext, useContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+export const useClimate = useContext(ClimateContext);
+
+const ClimateProvider = ({children}) => {
+    const [temp, setTemp] = useState(50);
+    return <ClimateContext.Provider
+    value={{ temp, setTemp}}>
+        {children}
+    </ClimateContext.Provider>
+}
+
+export default ClimateProvider;
+
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
